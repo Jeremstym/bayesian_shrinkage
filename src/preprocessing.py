fe2f55c15a637faee8e001d1226cbcc22a1c9b26
Adrien-Mcode <62481170+Adrien-Mcode@users.noreply.github.com>
2023-01-12 22:52:20 +0100

    + de preprocessing
    Création du fichiers avec mort / departement / jour de 2020

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -35,3 +35,22 @@ df2021['Location'] = df2021['Location'].apply(lambda x: x[14:])
 df2021.drop("Date", axis=1, inplace=True)
 df2021 = df2021.groupby(by='Location').count()
 df2021.to_csv('data/countdeath2021.csv')
+
+#Preprocessing for the departmental level : 
+
+df2020 = pd.read_csv('data/DC_2020_det.csv', delimiter=";", header = 0)
+
+df2020["DATDEC"] = pd.to_datetime([str(df2020.ADEC[i]) + "/" + str(df2020.MDEC[i]) + "/" + str(df2020.JDEC[i]) for i in range(df2020.shape[0])], format="%Y/%m/%d")
+
+df2020.drop(["ADEC","MDEC", "COMDEC","JDEC","ANAIS","MNAIS","JNAIS","SEXE","COMDOM","LIEUDEC2"], axis=1, inplace=True)
+
+#Dataframe by county / by day : 
+dep=pd.unique(df2020["DEPDEC"])
+date=pd.unique(df2020["DATDEC"])
+df2020["cst"] = [1 for i in range(df2020.shape[0])]
+df2020.set_index(["DATDEC","DEPDEC"], inplace = True)
+
+df_dep = pd.DataFrame({i:[int(df2020.loc[(i,j)].count()[0]) if (i,j) in df2020.index else 0 for j in dep] for i in date}, index = dep)
+#Now we have a dataframe with all the death by day by county
+
+df_dep.to_csv('data/countdeath2020.csv')
