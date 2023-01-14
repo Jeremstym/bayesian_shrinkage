a9a18f2d0aad9cf9efdffe7af64dcb3b9e683b36
Adrien-Mcode <62481170+Adrien-Mcode@users.noreply.github.com>
2023-01-14 13:34:20 +0100

    fin régression

--- src/model.py
+++ src/model.py
@@ -4,13 +4,14 @@
 # date: 12/01/2023
 
 import numpy as np
+import numpy.linalg as npln
 import numpy.random as nprd
 import scipy.stats as spst
 import scipy.special as spsp
 import statsmodels.api as sm
 
 from scipy.optimize import fsolve
-
+from scipy.linalg import sqrtm
 
 
 ## GLSP given adjusment term
@@ -168,7 +169,7 @@ def GLSP_count_reg(Y, X, offset=None, prior="EH", mc=3000, burn=500, HP= [1,1]) 
     
     ## initial values
     Lam = Nu = Y + 0.1
-    U = [1 for i in range(number_observation)]
+    u = [1 for i in range(number_observation)]
 
     if prior =="EH": 
         V = W = [1 for i in range(number_observation)]
@@ -177,88 +178,80 @@ def GLSP_count_reg(Y, X, offset=None, prior="EH", mc=3000, burn=500, HP= [1,1]) 
 
     Reg = sm.GLM(Y, X, family=sm.families.Poisson()).fit().params()
     Reg = [Reg[i] for i in Reg.index]
-
-    coef( glm(Y~X, offset=offset, family="poisson") )[-1]
     
     ## MCMC iterations
     for iteration in range(MC) :
         # Regression part
-        Q = function(b){  t(X)%*%(Y - Lam*exp(offset + X%*%b)) }
-        hReg = nleqslv(Reg, Q)$x   # mode
-        hmu = as.vector( Lam*exp(offset + X%*%hReg) )
-        mS = t(X*hmu)%*%X
-        A1 = solve( mS + Om )
-        A2 = as.vector( A1%*%( mS%*%hReg ) )
-        Reg.prop = mvrnorm(1, A2, A1)  # proporsal 
+        Q = lambda delta: X.T@(Y - Lam*np.exp(offset + X@delta))
+        hReg = fsolve(Q, Reg)   # mode
+        hmu = Lam*np.exp(offset + X@hReg) 
+        mS = (X@hmu).T@X
+        A1 = sqrtm(npln. inv(mS + Om))
+        #A2 = as.vector( A1%*%( mS%*%hReg ) )
+        Reg_prop = A1.T@nprd.randn(hReg.shape) + hReg  # proposal 
         
-        T1 = t(Y)%*%X%*%(Reg.prop-Reg) - sum( Lam*as.vector(exp(offset + X%*%Reg.prop) - exp(offset + X%*%Reg)) )
-        T2 = 0.5*( t(Reg.prop-hReg)%*%mS%*%(Reg.prop-hReg) - t(Reg-hReg)%*%mS%*%(Reg-hReg) )  
-        log.ratio = T1 + T2
-        pp = min(exp(log.ratio), 1)
-        ch = rbinom(1, 1, pp)
-        Reg = as.vector( Reg+ch*(Reg.prop-Reg) )
-        Eta = exp(offset + as.vector(X%*%Reg))    # adjustment term
-        Reg.pos[r,] = Reg
+        T1 = Y.T@X@(Reg_prop-Reg) - sum( Lam.T@np.exp(offset + X@Reg_prop) - np.exp(offset + X@Reg))
+        T2 = 0.5*((Reg_prop-hReg).T@mS@(Reg_prop-hReg) - (Reg-hReg).T@mS@(Reg-hReg))  
+        log_ratio = T1 + T2
+        pp = min(np.exp(log_ratio), 1)
+        ch = nprd.binomial(1,pp)
+        Reg = Reg+ch*(Reg_prop-Reg)
+        Eta = np.exp(offset + X@Reg)    # adjustment term
+        Reg_pos[iteration,:] = Reg
         
-        # Lambda
-        Lam = rgamma(m, Y+Alpha, Eta+Beta/U)
-        Lam.pos[r,] = Lam
+        lam = nprd.gamma(Y+alpha, 1/(Eta+beta/u), number_observation)
+        Lam_pos[iteration,:] = lam
+
+        # U and Gam (EH prior)
+        if prior == 'EH':
+            V = nprd.gamma(1+gam, 1+np.log(1+u), number_observation)
+            W = nprd.gamma(1+V, 1+u, number_observation)
+            
+            for i in range(number_observation):
+                u[i] = spst.geninvgauss(p=1-alpha, b=2*beta*lam[i]+2*W[i]).rvs()
+            
+            U_pos[iteration,:] = u
+            ss = np.sum(np.log(1+np.log(1+u)))
+            gam = nprd.gamma(HP[0]+number_observation, HP[1]+ss, 1)
+            Gam_pos[iteration] = gam
         
-        ## U and Gam (EH prior)
-        if( prior=="EH" ){
-        # U
-        V = rgamma(m, 1+Gam, 1+log(1+U))
-        W = rgamma(m, 1+V, 1+U)
-        for(i in 1:m){
-            U[i] = rgig(1, lambda=1-Alpha, chi=2*Beta*Lam[i], psi=2*W[i])
-        }
-        U.pos[r,] = U
-        # Gam
-        ss = sum( log(1+log(1+U)) )
-        Gam = rgamma(1, HP[1]+m, HP[2]+ss)
-        Gam.pos[r] = Gam
-        }
+        ## u and gam (IG prior)
+        if prior == "IG":
+            # u
+            u = spst.invgamma(a=alpha+gam).rvs(scale=beta*gam+lam, size=number_observation)
+            # gam
+            bb = 0.1
+            gam_new = min(max(gam+bb*nprd.normal(), 0.001), 150)
+            L1 = number_observation*gam*np.log(gam) - number_observation*np.log(spsp.gamma(gam)) - gam*np.sum(np.log(u)) - gam*np.sum(1/u)
+            L2 = number_observation*gam_new*np.log(gam_new) - number_observation*np.log(spsp.gamma(gam_new)) - gam_new*np.sum(np.log(u)) - gam_new*np.sum(1/u)
+            pp = min(np.exp(L2-L1),1)
+            gam = gam + nprd.binomial(1, pp)*(gam_new-gam)
+            Gam_pos[iteration] = gam
         
-        ## U and Gam (IG prior)
-        if( prior=="IG" ){
-        # U
-        U = rinvgamma(m, Alpha+Gam, Beta*Lam+Gam)
-        # Gam
-        bb = 0.1
-        Gam.new = min(max(Gam+bb*rnorm(1), 0.001), 150)
-        L1 = m*Gam*log(Gam) - m*log(gamma(Gam)) - Gam*sum(log(U)) - Gam*sum(1/U)
-        L2 = m*Gam.new*log(Gam.new) - m*log(gamma(Gam.new)) - Gam.new*sum(log(U)) - Gam.new*sum(1/U)
-        pp = min(exp(L2-L1), 1)
-        Gam = Gam + rbinom(1, 1, pp)*(Gam.new-Gam)
-        Gam.pos[r] = Gam
-        }
-        
-        # Beta
-        Beta = rgamma(1, HP[1]+m*Alpha, HP[2]+sum(Lam/U))
-        Beta.pos[r] = Beta
-        
+        # beta
+        beta = nprd.gamma(HP[0]+number_observation*alpha, HP[1]+np.sum(lam/u))
+        Beta_pos[iteration] = beta
+
         # alpha
-        Alpha = rgamma(1, HP[1]+sum(Nu), HP[2]+sum(log(1+Eta*U/Beta)))
-        Alpha.pos[r] = Alpha
-        
-        # Nu
-        for(i in 1:m){
-        if(Y[i]==0){ Nu[i]=0 }
-        if(Y[i]>0){
-            pp = Alpha/((1:Y[i])-1+Alpha)
-            Nu[i] = sum( rbinom(length(pp), 1, pp) )
-        }
-        }
-    }
+        alpha = nprd.gamma(HP[0]+np.sum(Nu), HP[1]+np.sum(np.log(1+Eta*u/beta)))
+        Beta_pos[iteration] = alpha
+
+        # nu
+        for i in range(number_observation):
+            if Y[i] == 0:
+                Nu[i] = 0
+            elif Y[i] > 0:
+                pp = alpha/(np.arange(1,Y[i]+1)-1+alpha)
+                Nu[i] = np.sum(nprd.binomial(1, pp, len(pp)))
     
     # Summary
-    om = 1:burn
-    Lam.pos = Lam.pos[-om,]
-    U.pos = U.pos[-om,]
-    Beta.pos = Beta.pos[-om]
-    Alpha.pos = Alpha.pos[-om]
-    Gam.pos = Gam.pos[-om]
-    Reg.pos = Reg.pos[-om,]
-    Res = list(lam=Lam.pos, u=U.pos, beta=Beta.pos, alpha=Alpha.pos, gam=Gam.pos, reg=Reg.pos)
-    if( prior=="PG" ){ Res = list(lam=Lam.pos, beta=Beta.pos, alpha=Alpha.pos, reg=Reg.pos) }
+    Lam_pos = Lam.pos[burn:,]
+    u_pos = u_pos[burn:,]
+    Beta_pos = Beta_pos[burn:]
+    Alpha_pos = Alpha_pos[burn:]
+    Gam_pos = Gam_pos[burn:]
+    Reg_pos = Reg_pos[burn:,]
+    Res = {"Lambda" : Lam_pos, "u" : u_pos, "Beta" : Beta_pos, "Alpha" : Alpha_pos, "Gamma" : Gam_pos, "Reg" : Reg_pos}
+    if prior=="PG":
+        Res = {"Lambda" : Lam_pos, "Beta" : Beta_pos, "Alpha" : Alpha_pos, "Reg" : Reg_pos}
     return(Res)
